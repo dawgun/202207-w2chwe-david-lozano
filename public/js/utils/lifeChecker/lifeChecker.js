551d6ed2d4aa3f90d0d216805319bf67e26e864f
David Lozano <dawgun@hotmail.com>
2022-07-16 22:22:09 +0200

    Modified variable counter to cellLivingCounter

--- public/js/utils/lifeChecker/lifeChecker.js
+++ public/js/utils/lifeChecker/lifeChecker.js
@@ -1,14 +1,14 @@
 const lifeChecker = (gameBoard, column, row) => {
-  const cellLivingCounter = 0;
+  let cellLivingCounter = 0;
 
-  if (gameBoard[column - 1][row - 1].life === true) counter += 1;
-  if (gameBoard[column - 1][row].life === true) counter += 1;
-  if (gameBoard[column - 1][row + 1].life === true) counter += 1;
-  if (gameBoard[column][row - 1].life === true) counter += 1;
-  if (gameBoard[column][row + 1].life === true) counter += 1;
-  if (gameBoard[column + 1][row - 1].life === true) counter += 1;
-  if (gameBoard[column + 1][row].life === true) counter += 1;
-  if (gameBoard[column + 1][row + 1].life === true) counter += 1;
+  if (gameBoard[column - 1][row - 1].life === true) cellLivingCounter += 1;
+  if (gameBoard[column - 1][row].life === true) cellLivingCounter += 1;
+  if (gameBoard[column - 1][row + 1].life === true) cellLivingCounter += 1;
+  if (gameBoard[column][row - 1].life === true) cellLivingCounter += 1;
+  if (gameBoard[column][row + 1].life === true) cellLivingCounter += 1;
+  if (gameBoard[column + 1][row - 1].life === true) cellLivingCounter += 1;
+  if (gameBoard[column + 1][row].life === true) cellLivingCounter += 1;
+  if (gameBoard[column + 1][row + 1].life === true) cellLivingCounter += 1;
 
   return cellLivingCounter;
 };
