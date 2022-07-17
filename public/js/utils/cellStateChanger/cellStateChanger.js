a41bb82f225f10d6ca9f5d83908b0ee701fbaae0
David Lozano <dawgun@hotmail.com>
2022-07-17 16:23:46 +0200

    Refactorized for correct testing

--- public/js/utils/cellStateChanger/cellStateChanger.js
+++ public/js/utils/cellStateChanger/cellStateChanger.js
@@ -1,12 +1,16 @@
 const cellStateChanger = (cellPosition, livingCells) => {
   const cellMutated = { ...cellPosition };
 
-  if (cellPosition.life === true && (livingCells < 2 || livingCells > 3))
+  if (cellPosition.life === true && (livingCells < 2 || livingCells > 3)) {
     cellMutated.life = false;
+    return cellMutated;
+  }
 
-  if (cellPosition.life === false && livingCells === 3) cellMutated.life = true;
-
-  return cellMutated;
+  if (cellPosition.life === false && livingCells === 3) {
+    cellMutated.life = true;
+    return cellMutated;
+  }
+  return undefined;
 };
 
 export default cellStateChanger;
