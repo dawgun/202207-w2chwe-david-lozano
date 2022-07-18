4a9d13925307de23a624102048ffc1475ce92027
David Lozano <dawgun@hotmail.com>
2022-07-18 02:23:30 +0200

    Fixed for correct generation array with parameters

--- public/js/utils/gridGenerator/gridGenerator.js
+++ public/js/utils/gridGenerator/gridGenerator.js
@@ -1,7 +1,7 @@
 const gridGenerator = (rowLength, columnLength) => {
-  const gameOfLifeBoard = new Array(3);
+  const gameOfLifeBoard = new Array(rowLength);
   for (let column = 0; column < columnLength; column += 1) {
-    gameOfLifeBoard[column] = new Array(3);
+    gameOfLifeBoard[column] = new Array(columnLength);
     for (let row = 0; row < rowLength; row += 1) {
       gameOfLifeBoard[column][row] = {
         position: `column${column} row${row}`,
